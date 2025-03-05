e2c0d16dcb5ad7d91eaf95158cf50c7d6e73b5ee
chinthaka senarathna <chironprabath@gmail.com>
2025-03-05 23:13:15 +0530

    first commit

---
+++ src/main/java/com/solohicker/solo_hicker/SoloHickerApplication.java
@@ -0,0 +1,13 @@
+package com.solohicker.solo_hicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloHickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloHickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solohicker/solo_hicker/SoloHickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solohicker.solo_hicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloHickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
